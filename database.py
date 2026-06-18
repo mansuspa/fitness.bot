ab4c18c2f18df00c6abd82d5241aa05455272de9
mansuspa <sashabelov_96@mail.ru>
2026-06-18 18:37:50 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,60 +1,32 @@
 import json
-import logging
-from datetime import datetime
-from config import DB_FILE
-
-logger = logging.getLogger(__name__)
-
 
 class Database:
-
     def __init__(self):
-        self.db_file = DB_FILE
-        self.users = self._load_users()
+        self.file = "db.json"
+        self.data = self.load()
 
-    def _load_users(self):
+    def load(self):
         try:
-            with open(self.db_file, "r", encoding="utf-8") as f:
+            with open(self.file, "r") as f:
                 return json.load(f)
         except:
             return {}
 
-    def _save(self):
-        with open(self.db_file, "w", encoding="utf-8") as f:
-            json.dump(self.users, f, ensure_ascii=False, indent=2)
+    def save(self):
+        with open(self.file, "w") as f:
+            json.dump(self.data, f, indent=2)
 
-    def add_user(self, user_id, username):
-        if str(user_id) not in self.users:
-            self.users[str(user_id)] = {
-                "username": username,
-                "goal": "maintain",
-                "weight": 70,
-                "height": 175,
-                "age": 25,
-                "gender": "male",
-                "weight_history": [],
-                "created_at": str(datetime.now())
-            }
-            self._save()
+    def add_user(self, uid, username):
+        uid = str(uid)
+        if uid not in self.data:
+            self.data[uid] = {"username": username, "goal": "maintain"}
+            self.save()
 
-    def get_user(self, user_id):
-        return self.users.get(str(user_id))
+    def get_user(self, uid):
+        return self.data.get(str(uid), {"goal": "maintain"})
 
-    def update_user(self, user_id, **kwargs):
-        uid = str(user_id)
-
-        if uid not in self.users:
-            return
-
-        for k, v in kwargs.items():
-            self.users[uid][k] = v
-
-        self._save()
-
-    def add_weight(self, user_id, weight):
-        uid = str(user_id)
-
-        if uid in self.users:
-            self.users[uid]["weight"] = weight
-            self.users[uid]["weight_history"].append(weight)
-            self._save()
+    def update_user(self, uid, **kwargs):
+        uid = str(uid)
+        if uid in self.data:
+            self.data[uid].update(kwargs)
+            self.save()
