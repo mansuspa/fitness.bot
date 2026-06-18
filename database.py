51781dea66af729e0e4573ede7307d455682bc25
mansuspa <sashabelov_96@mail.ru>
2026-06-18 15:17:29 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,69 +5,56 @@ from config import DB_FILE
 
 logger = logging.getLogger(__name__)
 
+
 class Database:
-    """Класс для работы с базой данных"""
-    
+
     def __init__(self):
         self.db_file = DB_FILE
         self.users = self._load_users()
-    
+
     def _load_users(self):
-        """Загрузка пользователей из файла"""
         try:
-            with open(self.db_file, 'r', encoding='utf-8') as f:
+            with open(self.db_file, "r", encoding="utf-8") as f:
                 return json.load(f)
-        except FileNotFoundError:
-            logger.info("Файл базы данных не найден, создаю новый")
+        except:
             return {}
-        except json.JSONDecodeError:
-            logger.error("Ошибка чтения файла базы данных")
-            return {}
-    
-    def _save_users(self):
-        """Сохранение пользователей в файл"""
-        try:
-            with open(self.db_file, 'w', encoding='utf-8') as f:
-                json.dump(self.users, f, ensure_ascii=False, indent=2)
-            logger.info(f"Сохранено {len(self.users)} пользователей")
-        except Exception as e:
-            logger.error(f"Ошибка сохранения базы данных: {e}")
-    
-    def add_user(self, user_id: int, username: str):
-        """Добавление пользователя"""
-        if user_id not in self.users:
-            self.users[user_id] = {
-                'user_id': user_id,
-                'username': username,
-                'created_at': datetime.now().isoformat(),
-                'weight': None,
-                'height': None,
-                'age': None,
-                'goal': None
+
+    def _save(self):
+        with open(self.db_file, "w", encoding="utf-8") as f:
+            json.dump(self.users, f, ensure_ascii=False, indent=2)
+
+    def add_user(self, user_id, username):
+        if str(user_id) not in self.users:
+            self.users[str(user_id)] = {
+                "username": username,
+                "goal": "maintain",
+                "weight": 70,
+                "height": 175,
+                "age": 25,
+                "gender": "male",
+                "weight_history": [],
+                "created_at": str(datetime.now())
             }
-            self._save_users()
-            logger.info(f"Пользователь {username} добавлен")
-    
-    def get_user(self, user_id: int):
-        """Получение информации о пользователе"""
-        return self.users.get(user_id)
-    
-    def update_user(self, user_id: int, **kwargs):
-        """Обновление информации о пользователе"""
-        if user_id in self.users:
-            for key, value in kwargs.items():
-                if key in self.users[user_id]:
-                    self.users[user_id][key] = value
-            self._save_users()
-            logger.info(f"Информация о пользователе {user_id} обновлена")
-    
-    def get_all_users(self):
-        """Получение списка всех пользователей"""
-        return list(self.users.values())
-    
-    def delete_user(self, user_id: int):
-        """Удаление пользователя"""
-        if user_id in self.users:
-            del self.users[user_id]
-            self._save_users()
-            logger.info(f"Пользователь {user_id} удалён")
+            self._save()
+
+    def get_user(self, user_id):
+        return self.users.get(str(user_id))
+
+    def update_user(self, user_id, **kwargs):
+        uid = str(user_id)
+
+        if uid not in self.users:
+            return
+
+        for k, v in kwargs.items():
+            self.users[uid][k] = v
+
+        self._save()
+
+    def add_weight(self, user_id, weight):
+        uid = str(user_id)
+
+        if uid in self.users:
+            self.users[uid]["weight"] = weight
+            self.users[uid]["weight_history"].append(weight)
+            self._save()
